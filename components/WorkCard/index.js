a488f8ac637148eabaf4c869bae23625f8ffb803
Aditya201514049 <adityasingha084@gmail.com>
2025-02-14 00:55:42 +0600

    refactor WorkCard component to improve image handling and enhance layout

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -31,6 +31,7 @@ const WorkCard = ({ img, name, description, onClick }) => {
 export default WorkCard;
 */
 
+/*
 import React from "react";
 import Image from "next/image";
 
@@ -48,6 +49,38 @@ const WorkCard = ({ img, name, description, onClick }) => {
           alt={name}
           src={img}
           layout="fill"
+          objectFit="cover"
+          className="hover:scale-105 transition-all ease-out duration-300"
+        />
+      </div>
+      <h1 className="mt-5 text-3xl font-medium">
+        {name ? name : "Project Name"}
+      </h1>
+      <h2 className="text-xl opacity-50">
+        {description ? description : "Description"}
+      </h2>
+    </div>
+  );
+};
+
+export default WorkCard;
+*/
+
+import React from "react";
+import Image from "next/image";
+
+const WorkCard = ({ img, name, description, onClick }) => {
+  return (
+    <div
+      className="overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link"
+      onClick={onClick}
+    >
+      <div className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto">
+        <Image
+          alt={name}
+          src={img}
+          width={500} // Set the desired width
+          height={300} // Set the desired height
           objectFit="cover"
           className="hover:scale-105 transition-all ease-out duration-300"
         />
